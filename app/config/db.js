047607ec3437975f1d1ad35e9fe0ff6924ab9e28
Llorente14 <axel.crisdy1980@gmail.com>
2025-04-14 21:03:22 +0700

    debug/db.js_env

--- app/config/db.js
+++ app/config/db.js
@@ -3,6 +3,7 @@ const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`ENV MONGODB: ${process.env.MONGODB_URI}`);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(err);
